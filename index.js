d0081013cd082d6ca2ba2a6526516c6cb0415645
Victoriia88 <vulynets528@gmail.com>
2023-04-26 21:29:12 +0300

    create homework 2

---
+++ index.js
@@ -0,0 +1,10 @@
+firstNumber = prompt ('Please enter the first number?');
+secondNumber = prompt ('Please enter the second number?');
+minus = (+firstNumber) - (+secondNumber);
+plus = (+firstNumber) + (+secondNumber);
+multiplication = (+firstNumber) * (+secondNumber);
+divide = (+firstNumber) / (+secondNumber);
+alert (firstNumber + '-' + secondNumber + '=' + minus);
+alert (firstNumber + '+' + secondNumber + '=' + plus);
+alert (firstNumber + '*' + secondNumber + '=' + multiplication);
+alert (firstNumber + '/' + secondNumber + '=' + divide);
