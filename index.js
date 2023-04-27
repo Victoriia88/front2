f9502e049cc04dc3ae8da9f769069b3293e78110
Victoriia88 <vulynets528@gmail.com>
2023-04-27 22:03:21 +0300

    create homework2

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-firstNumber = prompt ('Please enter the first number?');
-secondNumber = prompt ('Please enter the second number?');
-minus = (+firstNumber) - (+secondNumber);
-plus = (+firstNumber) + (+secondNumber);
-multiplication = (+firstNumber) * (+secondNumber);
-divide = (+firstNumber) / (+secondNumber);
-alert (firstNumber + '-' + secondNumber + '=' + minus);
-alert (firstNumber + '+' + secondNumber + '=' + plus);
-alert (firstNumber + '*' + secondNumber + '=' + multiplication);
-alert (firstNumber + '/' + secondNumber + '=' + divide);
+// firstNumber = prompt ('Please enter the first number?');
+// secondNumber = prompt ('Please enter the second number?');
+// minus = (+firstNumber) - (+secondNumber);
+// plus = (+firstNumber) + (+secondNumber);
+// multiplication = (+firstNumber) * (+secondNumber);
+// divide = (+firstNumber) / (+secondNumber);
+// alert (firstNumber + '-' + secondNumber + '=' + minus);
+// alert (firstNumber + '+' + secondNumber + '=' + plus);
+// alert (firstNumber + '*' + secondNumber + '=' + multiplication);
+// alert (firstNumber + '/' + secondNumber + '=' + divide);
+
+firstRow = prompt ('Please enter your first name?')
+secondRow = prompt ('Please enter your second name?')
+thirthRow = prompt ('Please enter your nationality?')
+console.log(firstRow + ' ' + secondRow + ' ' + thirthRow);
+
+numbers = prompt ('Please enter 5 numbers?')
+newNumbers = numbers.toString().replaceAll('', ' ');
+console.log(newNumbers);
