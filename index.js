bdd012979f20079be91ea25040091e7a05438aaf
Victoriia88 <vulynets528@gmail.com>
2023-04-29 23:04:15 +0300

    create homework 5

--- index.js
+++ index.js
@@ -9,11 +9,19 @@
 // alert (firstNumber + '*' + secondNumber + '=' + multiplication);
 // alert (firstNumber + '/' + secondNumber + '=' + divide);
 
-firstRow = prompt ('Please enter your first name?')
-secondRow = prompt ('Please enter your second name?')
-thirthRow = prompt ('Please enter your nationality?')
-console.log(firstRow + ' ' + secondRow + ' ' + thirthRow);
 
-numbers = prompt ('Please enter 5 numbers?')
-newNumbers = numbers.toString().replaceAll('', ' ');
-console.log(newNumbers);
+// firstRow = prompt ('Please enter your first name?')
+// secondRow = prompt ('Please enter your second name?')
+// thirthRow = prompt ('Please enter your nationality?')
+// console.log(firstRow + ' ' + secondRow + ' ' + thirthRow);
+
+// numbers = prompt ('Please enter 5 numbers?')
+// newNumbers = numbers.toString().replaceAll('', ' ');
+// console.log(newNumbers);
+
+firstNub = parseFloat (prompt ('Please enter your first number?'));
+secondtNub = parseFloat (prompt ('Please enter your second number?'));
+thirthNub = parseFloat (prompt ('Please enter your thirth number?'));
+
+averageNum = (firstNub + secondtNub + thirthNub) / 3;
+alert(`Your average number is  ${averageNum} !`);
